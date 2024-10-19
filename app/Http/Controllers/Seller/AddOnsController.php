9e98b0c13e465c96d915da5c2c90909a13cc9dae
brijesh <brijeshbrijesh36262@gmail.com>
2024-10-19 12:19:54 +0530

    done

--- app/Http/Controllers/Seller/AddOnsController.php
+++ app/Http/Controllers/Seller/AddOnsController.php
@@ -151,12 +151,10 @@ class AddOnsController extends Controller
 public function showItems(Request $request)
 {
     $query = $request->get('search');
-    
-    
+
     if ($query) {
         $items = AddOnItem::where('name', 'like', "%{$query}%")
                           ->orWhere('description', 'like', "%{$query}%")
-                          ->with('category')
                           ->with('category')
                           ->get();
     } else {
@@ -208,68 +206,11 @@ public function showItems(Request $request)
             ';
         }
 
-        return response()->json([
-            'html' => $html,
-        ]);
+        return response()->json(['html' => $html]);
     }
 
     return view('seller.addOns.Items', compact('items'));
 }
-    // Return the HTML for the table rows via AJAX
-    if ($request->ajax()) {
-        $html = '';
-        foreach ($items as $item) {
-            $html .= '
-                <tr class="border-t border-gray-200">
-                    <td class="py-4">
-                        <div class="flex items-center">
-                            <img src="' . Storage::url($item->image) . '" alt="' . $item->name . '" class="w-10 h-10 rounded-full mr-3">
-                            <span class="text-gray-700">' . $item->name . '</span>
-                        </div>
-                    </td>
-                    <td class="py-4">' . $item->category->name . '</td>
-                    <td class="py-4">$' . $item->price . '</td>
-                    <td class="py-4">
-                        <label class="flex items-center cursor-pointer">
-                            <div class="relative">
-                                <input type="checkbox" class="sr-only" ' . ($item->status ? 'checked' : '') . '>
-                                <div class="w-10 h-6 bg-gray-200 rounded-full shadow-inner"></div>
-                                <div class="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition"></div>
-                            </div>
-                        </label>
-                    </td>
-                    <td class="py-4">
-                        <div class="flex space-x-2">
-                            <a href="' . route('items.edit', $item->id) . '" class="text-indigo-600 hover:text-indigo-900">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                                </svg>
-                            </a>
-                            <form action="' . route('items.destroy', $item->id) . '" method="POST">
-                                ' . csrf_field() . '
-                                ' . method_field('DELETE') . '
-                                <button class="text-red-600 hover:text-red-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            ';
-        }
-
-        return response()->json([
-            'html' => $html,
-        ]);
-    }
-
-    return view('seller.addOns.Items', compact('items'));
-}
-
-
-
 
     public function editItems($id)
     {
